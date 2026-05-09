cbdf5d15b1ca24419380562b770d3161b06ec8fb
Mazen El-Mallah <mazenelmallah@gmail.com>
2026-05-10 02:38:37 +0300

    Delete functions/translate.py

--- functions/translate.py
+++
@@ -1,4 +0,0 @@
-def translate_image(img_array, dx, dy):
-    pil_img = Image.fromarray(img_array)
-    translated = pil_img.transform(pil_img.size, Image.AFFINE, (1, 0, -dx, 0, 1, -dy))
-    return translated
