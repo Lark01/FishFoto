550d3d499ecbc146c5094eb235e17fa30d50b79b
Mazen El-Mallah <mazenelmallah@gmail.com>
2026-05-10 02:34:37 +0300

    Delete functions/blur.py

--- functions/blur.py
+++
@@ -1,5 +0,0 @@
-def blur_image(image, sigma, kernel_size):
-    kernel = cv2.getGaussianKernel(kernel_size, sigma)
-    kernel = kernel @ kernel.T
-    blurred_image = cv2.filter2D(image, -1, kernel)
-    return blurred_image
